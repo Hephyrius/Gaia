031b1a8024b66be253404d438a26399657759b92
Harnick Khera <Hephyrius@gmail.com>
2018-07-22 07:51:21 +0100

    Added Initialisation Function

--- Core/NeuralSwarm.py
+++ Core/NeuralSwarm.py
@@ -4,14 +4,19 @@ Created on Sun Jul 22 07:34:01 2018
 
 @author: Harnick Khera
 """
+import random as r
+import FeedForwardNetwork as FFN
 
 class NeuralSwarm():
-    
+    #Hyper Parameters
     InputSize = 0
     OutputSize = 0
     SwarmSize = 0
     TotalSwarms = 0
-    SwarmSizes = []
+    
+    #store population networks and swarm sizes
+    SwarmPopulations = []
+    SwarmNetworkSizes = []
     
     def __init__(self, _InputSize, _OutputSize, _SwarmSize, _TotalSwarms):
         
@@ -19,9 +24,36 @@ class NeuralSwarm():
         self.OutputSize = _OutputSize
         self.SwarmSize = _SwarmSize
         self.TotalSwarms = _TotalSwarms
-        self.SwarmSizes = []
+        self.SwarmPopulations = []
+        self.SwarmNetworkSizes = []
         
-        
+        #initialise the networks in the swarms
+        self.initSwarms()
+    
+    #create random sized networks and add them to a population
     def initSwarms(self):
         
-        
+        for i in range(self.TotalSwarms):
+            NetworkSize = []
+            NetworkSize.append(self.InputSize)
+            HiddenSize = r.randint(1,2)
+            for j in range(HiddenSize):
+                NetworkSize.append(r.randint(2,(8*self.InputSize)))
+                
+            NetworkSize.append(self.OutputSize)
+            
+            self.SwarmNetworkSizes.append(NetworkSize)
+            
+            Population = []
+            
+            for j in range(self.SwarmSize):
+                
+                partical = FFN.FeedForwardNetwork(NetworkSize)
+                Population.append(partical)
+            
+            self.SwarmPopulations.append(Population)
+            
+HiveMind = NeuralSwarm(3,2,100,1)            
+            
+
+            
