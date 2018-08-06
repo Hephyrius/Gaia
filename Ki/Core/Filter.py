0bc9b4f3774d2b14b97e778b4aabc16517858f28
Harnick Khera <Hephyrius@gmail.com>
2018-08-06 23:11:28 +0100

    Filter Application now working

--- Ki/Core/Filter.py
+++ Ki/Core/Filter.py
@@ -6,25 +6,77 @@ Created on Thu Aug  2 18:42:15 2018
 """
 
 import numpy as np
+#from scipy import signal
 import random as r
 
 #Filter to be used as part of a Convolution Networks' Node
 class Filter():
     
     Filter = []
-    FilterX = 0
-    FilterY = 0
-    Stride = 0
-    Padding = 0
+    FilterSize = []
+    Stride = []
+    Padding = []
     
-    def __init__(self, _FilterX=3, _FilterY=3, _Stride=1, _Padding=1):
+    def __init__(self, _FilterSize=[3,3], _Stride=[1,1], _Padding=[1,1,1,1]):
         
-        self.FilterX = _FilterX
-        self.FilterY = _FilterY
+        self.FilterSize = _FilterSize
         self.Stride = _Stride
         self.Padding = _Padding
-        #self.Filter = np.zeros([self.FilterX, self.FilterY])
-        self.Filter = np.random.uniform(low =-1, high=1, size=[self.FilterX, self.FilterY])
+
+        self.Filter = np.random.uniform(low =-1, high=1, size=[self.FilterSize[0], self.FilterSize[1]])
     
-fil = Filter(15,15)
-print(fil.Filter)
+    #Apply a filter to some input data
+    def ApplyFilter(self, InputArray):
+        
+        xStride = int(InputArray.shape[0]/self.Stride[0])
+        yStride = int(InputArray.shape[1]/self.Stride[1])
+        
+        filterOutput = np.zeros([xStride,yStride])
+        
+        #Apply the filter to the input
+        for i in range(xStride):
+            for j in range(yStride):
+                
+                padded = np.zeros([self.FilterSize[0], self.FilterSize[1]])                
+                cube = InputArray[i:i+self.FilterSize[0],j:j+self.FilterSize[1]]
+                
+                padded[:cube.shape[0], :cube.shape[1]] = cube
+                Filtered = padded*self.Filter
+                filterOutput[i,j] = np.sum(Filtered)
+        
+        return filterOutput
+        
+
+#Test Data of a Cube
+data = [[1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1]]
+
+data = np.asarray(data)
+
+#Multiple filters
+filts = []
+outs = []
+for i in range(5):
+    
+    filts.append(Filter())
+    out = filts[i].ApplyFilter(data)
+    outs.append(out)
+    print(out)
+
+
+
+
+
+
+
+
+
+
+
