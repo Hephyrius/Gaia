36fe598888fb2984a04b9bd55ef5c103f940f7f5
Harnick Khera <Hephyrius@gmail.com>
2018-08-14 19:40:41 +0100

    Initial Components of a Fuzzy Inference/Anfis Model

---
+++ Ki/Fuzzy/MembershipFunctions.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 14 19:19:24 2018
+
+@author: Khera
+"""
+
+class MembershipFunction():
+    
+    inited = False
+    def __init__(self):
+        self.inited = True
+    
+    
+    
