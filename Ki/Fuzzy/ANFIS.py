4a72dc93797e50e1af94e211ffe9877a7b2e51bd
Harnick Khera <Hephyrius@gmail.com>
2018-08-14 20:33:36 +0100

    Initial ANFIS Starter

---
+++ Ki/Fuzzy/ANFIS.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 14 20:22:55 2018
+
+@author: Harnick Khera
+
+ANFIS Implementation
+"""
+
+import numpy as np
+import skfuzzy as fz
+import MembershipFunctions as mf
+
+class anfis():
+    
+    i = 0
+    
+    def __init__(self):
+        self.i = 0
+
+
+
+a = anfis()
