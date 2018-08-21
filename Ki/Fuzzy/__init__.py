77346975f4bec645cce7db7a947f471189cf18ef
Harnick Khera <Hephyrius@gmail.com>
2018-08-21 23:04:09 +0100

    updaterd whl related files

---
+++ Ki/Fuzzy/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 21 22:50:01 2018
+
+@author: Harnick Khera
+"""
+
+from .MembershipFunction import MembershipFunction
--- Ki/__init__.py
+++ Ki/__init__.py
@@ -6,5 +6,6 @@ Created on Tue Jul 24 20:17:19 2018
 """
 from __future__ import absolute_import
 from . import Core
+from . import Fuzzy
 
 __version__ = '0.2'
