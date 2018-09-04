cad2d82787e1ae6f3c47673a1d020fb53881f1bc
Harnick Khera <Hephyrius@gmail.com>
2018-09-04 09:43:37 +0100

    created a heuristic class that creates features

---
+++ Ki/Feature/GeneticFeatureCreation.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  4 09:40:23 2018
+
+@author: Khera
+
+genetic feature creation uses GA to create features using strings of mathematical operations
+"""
+
+import pandas as pd
+import numpy as np
+import random as r
+
+class GeneticFeatureCreation():
